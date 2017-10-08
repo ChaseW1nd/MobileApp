20c9385fa159f2183f23ca8b6cbdc9a2371c29d8
Ellen Gong <zgong1@student.unimelb.edu.au>
2017-10-08 15:44:01 +1100

    Test connection [success]

--- MobileApp/RegisterViewController.swift
+++ MobileApp/RegisterViewController.swift
@@ -13,7 +13,7 @@ class RegisterViewController: UIViewController {
 
     // TODO: URL for server
     
-    let URL_USER_REGISTER = ""
+    let URL_USER_REGISTER = "http://10.13.2.137:8181/register"
     
     // MARK: Properties
     
@@ -52,8 +52,9 @@ class RegisterViewController: UIViewController {
 //        let name = nameField.text
 //        let phone = phoneField.text
         
-        if isValidPhone(value: phoneField.text!) {
-            
+        //if isValidPhone(value: phoneField.text!) {
+        // FIXME: For testing
+        if true {
             // Phone number is valid.
             debugPrint("All good", phoneField.text ?? "null")
             
@@ -66,29 +67,53 @@ class RegisterViewController: UIViewController {
             ]
             
             // Sending http post request.
-            Alamofire.request(URL_USER_REGISTER, method: .post, parameters: parameters, encoding: JSONEncoding.default)
-                // FIXME: statusCode should align with the server implementation.
-                .validate(statusCode: 200..<300)
-                .responseJSON {
-                    response in
-                    switch response.result {
-                    
-                    // TODO: Test connecting.
-                    case .success:
-                        // Print response.
-                        debugPrint(response)
-                    
-                        // Convert it as NSDictionary.
-                        let jsonData = response.result.value as! NSDictionary
-                        
-                        // Way to access: jsonData.value(forKey: "message") as! String?
-                        // jsonData.value(forKey: "sessionID") as! String?
-                        
-                    case .failure(let error):
-                        debugPrint(error)
+            Alamofire.request(URL_USER_REGISTER, method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+                print("Request: \(String(describing: response.request))")   // original url request
+                print("Response: \(String(describing: response.response))") // http url response
+                print("Result: \(response.result)")                         // response serialization result
+                
+                if let json = response.result.value {
+                    print("JSON: \(json)") // serialized json response
                 }
+                
+                if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                    print("Data: \(utf8Text)") // original server data as UTF8 string
                     
+                }
+                
+                let jsonData = response.result.value as! NSDictionary
+                print(jsonData.value(forKey: "test") as! NSDecimalNumber?)
+                
             }
+            
+            
+//            Alamofire.request(URL_USER_REGISTER, method: .post, parameters: parameters
+//                //, encoding: JSONEncoding.default
+//                )
+//                // FIXME: statusCode should align with the server implementation.
+//                //.validate(statusCode: 200..<300)
+//                .responseString {
+//                    response in
+//                    switch response.result {
+//                    
+//                    // TODO: Test connecting.
+//                    case .success:
+//                        // Print response.
+//                        debugPrint(response)
+//                    
+//                        // Convert it as NSDictionary.
+//                        //let jsonData = response.result.value as! NSDictionary
+//                        
+//                        //print(response.result.value(forKey: "test"))
+//                        
+//                        // Way to access: jsonData.value(forKey: "message") as! String?
+//                        // jsonData.value(forKey: "sessionID") as! String?
+//                        
+//                    case .failure(let error):
+//                        debugPrint(error)
+//                }
+//                    
+//            }
             
             
         
